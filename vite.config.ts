03bf8cd96493f7da748ea43ebfaaca67b64c25fb
Jai Kumar <navjyoti.rs@gmail.com>
2026-07-23 17:42:28 +0530

    Add .htaccess to build for GoDaddy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { writeFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'htaccess-for-apache',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: '.htaccess',
+          source: `<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+  RewriteRule ^index\\.html$ - [L]
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteRule . /index.html [L]
+</IfModule>
+`,
+        })
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
